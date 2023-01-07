026d3fec41b1908730e92b56e72f379ed6b620e9
Al Natour Mazen <115185120+Al-Natour-Mazen@users.noreply.github.com>
2023-01-08 00:55:50 +0100

    Add files via upload

---
+++ JeuPendu_Project/fonctions.py
@@ -0,0 +1,85 @@
+﻿from donnees import *
+from random import choice
+import turtle as tt
+import time
+
+
+def nom_joueur():
+    nom=input("Entrez votre nom : \t")
+    if len(nom)<3:
+        nom=input("Entrez un nom valide svp")
+        print("Good luck",nom,"!Try to find the right word to win.")
+        return nom_joueur()
+    else:
+        print("Good luck",nom,"!Try to find the right word to win.")
+
+def lettre_recup():
+    lettre_entree = input("Tapez une lettre seulement: ")
+    if len(lettre_entree)>1:
+        return lettre_entree[0]
+    else:
+        return lettre_entree
+
+def mot_a_choisir():
+    return choice(liste_mot)
+
+def mot_a_cacher(mot_a_chercher,lettre_trouvees):
+    mot_cacher=""
+    for lettre_entrer in mot_a_chercher:
+        if lettre_entrer in lettre_trouvees:
+            mot_cacher+= lettre_entrer
+        else:
+            mot_cacher += "*"
+    return mot_cacher
+
+def affiche(nombre_chance):
+    try:
+        tt.reset()
+    except tt.Terminator:
+        pass
+    tt.hideturtle()
+    if nombre_chance<=9 :
+     tt.penup()
+     tt.goto(-100,-100)
+     tt.pendown()
+     tt.forward(150)
+    if nombre_chance<=8 :
+     tt.penup()
+     tt.goto(-75,-100)
+     tt.pendown()
+     tt.goto(-75,100)
+    if nombre_chance<=7 :
+     tt.pendown()
+     tt.goto(-75,100)
+     tt.forward(200)
+    if nombre_chance<=6 :
+     tt.goto(125,80)
+    if nombre_chance<=5 :
+     tt.penup()
+     tt.goto(125,50)
+     tt.pendown()
+     tt.circle(15)
+    if nombre_chance<=4 :
+     tt.penup()
+     tt.goto(125,50)
+     tt.pendown()
+     tt.goto(125,-30)
+    if nombre_chance<=3 :
+     tt.goto(150,-60)
+     tt.penup()
+     tt.goto(125,-30)
+    if nombre_chance<=2:
+     tt.pendown()
+     tt.goto(100,-60)
+     tt.penup()
+     tt.goto(125,10)
+    if nombre_chance<=1 :
+     tt.pendown()
+     tt.goto(90,20)
+     tt.penup()
+     tt.goto(125,10)
+    if nombre_chance<=0 :
+     tt.pendown()
+     tt.goto(160,20)
+    time.sleep(3)
+    tt.bye()
---
+++ JeuPendu_Project/jeu.py
@@ -0,0 +1,39 @@
+﻿from donnees import *
+from fonctions import *
+
+
+MESSAGE_GAGNE = "BRAVO, vous avez trouvé le mot !!! Vous avez remporté la partie !"
+MESSAGE_PERDU = "DOMMAGE, vous n'avez pas trouvé le mot... Vous avez perdu la partie."
+
+def jouer_partie():
+    mot_a_chercher = mot_a_choisir()
+    lettres_trouvees = []
+    mot_trouve = False
+    nombre_chance = 10
+
+    while not mot_trouve and nombre_chance > 0:
+        print("\n----------------------------------------------------\n")
+        print("Mot à trouver:", mot_a_cacher(mot_a_chercher, lettres_trouvees))
+        lettre = lettre_recup()
+        if lettre in mot_a_chercher:
+            lettres_trouvees.append(lettre)
+            print("BRAVO, vous avez trouvé une lettre, il vous reste", nombre_chance, "chance(s)")
+        else:
+            nombre_chance -= 1
+            print("NON, cette lettre n’appartient pas au mot, il vous reste", nombre_chance, "chance(s)")
+            print("Regardez la fênetre Turtle (fênetre plume), pour voir l'état de votre pendu ;), attendez 8s pour continuer!!")
+            affiche(nombre_chance)
+        mot_trouve = mot_a_chercher == mot_a_cacher(mot_a_chercher, lettres_trouvees)
+    if mot_trouve:
+        print(MESSAGE_GAGNE)
+    else:
+        print(MESSAGE_PERDU)
+
+def jouer():
+    jouer_encore = True
+    utilisateur = nom_joueur()
+    while jouer_encore:
+        jouer_partie()
+        jouer_encore = input("Voulez-vous rejouer (oui/non)?").lower() == "oui"
+
+jouer()
